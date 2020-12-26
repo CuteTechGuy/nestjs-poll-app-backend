0ff4493b0e6444ecbf30190d86232ac78e472eff
ragecub3 <mail@ibrahimalkan.com>
2020-12-26 23:58:31 +0300

    healthCheck added

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,10 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return status ok and timestamp', () => {
+      const response = appController.healthCheck();
+      expect(response.status).toBe('ok');
+      expect(response.time).toBeDefined();
     });
   });
 });
