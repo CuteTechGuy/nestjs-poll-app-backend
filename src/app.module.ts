8cd366f88c7afd9030773570563a12e279f30684
ragecub3 <mail@ibrahimalkan.com>
2020-12-26 23:05:28 +0300

    config module and .env file added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017')],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot('mongodb://localhost:27017'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
